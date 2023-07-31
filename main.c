25d32734c9cfe7f774664fd07fde0a78fa6692d7
peutt <aymericqueret@gmail.com>
2023-07-31 17:11:31 +0200

    camembert avec label et trait

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 #include <string.h>
 #include <gd.h>
 #include <math.h>
+#include <time.h>
 
-#define Longueur 600
-#define Largeur 400
+#define Taille 600
 #define Taille_Label 12
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
     if (argc < 3) {
-        printf("Usage: %s <nom_fichier.png> <percentage1> <label1> <percentage2> <label2> ... <percentageN> <labelN>\n", argv[0]);
+        printf("Usage: %s <nom_fichier.png> <pourcentage1> <label1> <pourcentage2> <label2> ... <pourcentageN> <labelN>\n", argv[0]);
         return 1;
     }
     const char *nomFichier = argv[1];
@@ -22,41 +24,83 @@ int main(int argc, char *argv[]) {
     }
 
     // Cree une image fond blanc
-    gdImagePtr image = gdImageCreate(Longueur, Largeur);
+    gdImagePtr image = gdImageCreate(Taille, Taille);
     int blanc = gdImageColorAllocate(image, 255, 255, 255);
     int noir = gdImageColorAllocate(image, 0, 0, 0);
     gdImageFill(image, 0, 0, blanc);
 
     // Dessine le Camembert
-    int x = Longueur / 2;
-    int y = Largeur / 2;
+    int x = Taille / 2;
+    int y = Taille / 2;
+    int diametre = Taille / 2;
+    gdImageSetThickness(image, 5);
+    gdImageArc(image, x, y, x, y, 0, 360, noir);
+    gdImageSetThickness(image, 1);
     int angleDebut = 0;
 
     for (int i = 2; i < argc; i += 2) {
+
+        //Dessine le cartier du camembert en couleur
         double pourcentage = atof(argv[i]);
         int couleur = gdImageColorAllocate(image, rand() % 256, rand() % 256, rand() % 256);
         int angleFin = angleDebut + (360 * pourcentage / totalPourcentage);
-        gdImageFilledArc(image, x, y, Longueur / 2, Largeur / 2, angleDebut, angleFin, couleur, gdPie);
+        gdImageFilledArc(image, x, y, diametre, diametre, angleDebut, angleFin, couleur, gdPie);
+
+        //Dessine la bordure noir du camembert
+        gdImageSetThickness(image, 4);
+        gdImageArc(image, x, y, diametre, diametre, angleDebut, angleFin, noir);
+
+        // Dessine les bord noir entre les segments
+        int startX = x + diametre/2 * cos(angleDebut * M_PI / 180);
+        int startY = y + diametre/2 * sin(angleDebut * M_PI / 180);
+        gdImageLine(image, x, y, startX, startY, noir);
+        int endX = x + diametre/2 * cos(angleFin * M_PI / 180);
+        int endY = y + diametre/2 * sin(angleFin * M_PI / 180);
+        gdImageLine(image, x, y, endX, endY, noir);
+
+        gdImageSetThickness(image, 1);
+
+        // Calcul la position du label
+        double midAngle = (angleDebut + angleFin) / 2.0;
+        double rayonLabel;
+        if(cos(midAngle * M_PI / 180)<0){
+            rayonLabel= diametre/1.6 + strlen(argv[i + 1])*(Taille_Label/1.6);
+        } else {
+            rayonLabel= diametre/1.6;
+        }
+        int labelX = x + rayonLabel * cos(midAngle * M_PI / 180);
+        int labelY = y + diametre/1.6 * sin(midAngle * M_PI / 180);
+
+        // Ecrit le label
+        gdImageStringFT(image, NULL, gdImageColorAllocate(image, 0, 0, 0), "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", Taille_Label, 0, labelX, labelY, argv[i + 1]);
+
+        // dessine le trait entre le label et le camembert
+        int xStart = x + diametre/2 * cos(midAngle * M_PI / 180);
+        int yStart = y + diametre/2 * sin(midAngle * M_PI / 180);
+        if(cos(midAngle * M_PI / 180)<0){
+            float increment=strlen(argv[i + 1])*(Taille_Label/1.3);
+            printf("sin: %f, cos: %f\n", sin(midAngle * M_PI / 180), cos(midAngle * M_PI / 180));
+            if(sin(midAngle * M_PI / 180)>0){
+                gdImageLine(image, xStart, yStart, labelX+increment-sin(midAngle * M_PI / 180)*increment, labelY, noir);
+            } else {
+                gdImageLine(image, xStart, yStart, labelX+increment+sin(midAngle * M_PI / 180)*increment, labelY, noir);
+            }
+
+        } else {
+            gdImageLine(image, xStart, yStart, labelX, labelY, noir);
+        }
+
         angleDebut = angleFin;
+  }
 
-        // Calcul de la position du label
-        double labelAngle = (angleDebut + angleFin) / 2.0;
-        int labelX = x + (Longueur / 4) * cos(labelAngle * M_PI / 180);
-        int labelY = y + (Largeur / 4) * sin(labelAngle * M_PI / 180);
+  // Sauvegarde l'image dans le nomFichier
+  FILE *fichierSortie = fopen(nomFichier, "wb");
+  gdImagePng(image, fichierSortie);
+  fclose(fichierSortie);
 
-        // Dessine le label sur l'image
-        gdImageStringFT(image, NULL, noir, "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", Taille_Label, 0, labelX, labelY, argv[i + 1]);
-    }
+  // Libère la mémoire
+  gdImageDestroy(image);
+  printf("Le Camembert s'est généré et sauvegardé dans %s\n", nomFichier);
 
-    // Sauvegarde l'image dans le nomFichier
-    FILE *fichierSortie = fopen(nomFichier, "wb");
-    gdImagePng(image, fichierSortie);
-    fclose(fichierSortie);
-
-    // Libère la mémoire
-    gdImageDestroy(image);
-
-    printf("Le Camembert s'est généré et sauvegardé dans %s\n", nomFichier);
-
-    return 0;
+  return 0;
 }
